845b9dade0dee2ce617708b8dd8fe741b36adced
denish-sojitra <denishsojitra2346@gmail.com>
2025-06-30 16:14:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,6 @@ def translate_and_overlay():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))  # required for Render
+    app.run(debug=False, host="0.0.0.0", port=port)
+
